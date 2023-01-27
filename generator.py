8a44318f721734bf9a340bb8a7a9485ecdfa2057
Jhoner Santiago Novoa Rojas <jhoner300@gmail.com>
2023-01-26 23:26:42 -0500

    update pep 8 - black

--- generator.py
+++ generator.py
@@ -2,34 +2,34 @@ import pandas as pd
 import numpy as np
 
 
+# """This part we will found the code will  convert the sentiment
+#    data in a generator to meke the reuest to the model """
 
-#"""This part we will found the code will  convert the sentiment 
-#    data in a generator to meke the reuest to the model """    
 
 class Generator:
-    def __init__(self, Sentiment,Path):
-        
+    def __init__(self, Sentiment, Path):
+
         self.Sentiment = Sentiment
         self.Path = Path
-        
+
     def Iterator(self):
-        
+
         df = self.Sentiment
-        
-        Sentiment_df = df['Message']
-        
+
+        Sentiment_df = df["Message"]
+
         General_list = Sentiment_df.to_numpy().tolist()
-        
-        New_df = pd.DataFrame (General_list, columns = ['Sentiment'])
-        
+
+        New_df = pd.DataFrame(General_list, columns=["Sentiment"])
+
         for i in range(len(New_df)):
-            
-            df_fn = (New_df.loc[i, 'Sentiment'])
-            
+
+            df_fn = New_df.loc[i, "Sentiment"]
+
             new_list = [df_fn]
-            
-            df_second = pd.DataFrame(new_list, columns = [''])
-            
-            df_second.to_csv(f"{self.Path}{i}.txt",index=False,header=False)
-        
-        return New_df 
+
+            df_second = pd.DataFrame(new_list, columns=[""])
+
+            df_second.to_csv(f"{self.Path}{i}.txt", index=False, header=False)
+
+        return New_df
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from params import*
-from input_data import*
-from data_frame import*
-from generator import*
-from generator_path import*
+from params import *
+from input_data import *
+from data_frame import *
+from generator import *
+from generator_path import *
 import pandas as pd
 import numpy as np
 from google.cloud import aiplatform
-#from google.cloud.aiplatform.gapic.schema import predict
-#from google.protobuf import json_format
-#from google.protobuf.struct_pb2 import Value
+
+# from google.cloud.aiplatform.gapic.schema import predict
+# from google.protobuf import json_format
+# from google.protobuf.struct_pb2 import Value
 from typing import Sequence, Union
 import gcsfs
 
@@ -21,8 +22,8 @@ def batch_prediction_job(
     gcs_source: Union[str, Sequence[str]],
     gcs_destination: str,
     sync: bool = True,
-    ):
-    
+):
+
     aiplatform.init(project=project, location=location)
 
     my_model = aiplatform.Model(model_resource_name)
@@ -35,47 +36,44 @@ def batch_prediction_job(
     )
 
 
-def run(event,context):
-    
-    #Call Params to find read data
+def run(event, context):
+
+    # Call Params to find read data
     Params = Param()
-    filename = event['name'] 
-    input_bucket = event['bucket']
-    
-    Route_m = "gs://{0}/{1}".format(input_bucket,filename)
+    filename = event["name"]
+    input_bucket = event["bucket"]
+
+    Route_m = "gs://{0}/{1}".format(input_bucket, filename)
     print(f"The Params is: Ok")
-    
-    
-    #Extract the Data 
+
+    # Extract the Data
     All_Data = Input_Data(Route_m)
     Data = All_Data.Data_Sent()
     print(f"The Data is: Ok")
-    
-    
-    #Extract the Sentiment 
+
+    # Extract the Sentiment
     Extract = Data_Frame(Data)
     Data_Sentiment = Extract.Transform()
     print(f"The sentiment export to list is: Ok")
-    
-    
-    #Generate Data files txt
+
+    # Generate Data files txt
     Variable_S = Generator(Data_Sentiment, Params.Processing_Batch)
     Sentiments = Variable_S.Iterator()
-    print (f"The sentiment already exported to txt : Ok ")
- 
-    
-    #Generate list with paths of sentiments 
+    print(f"The sentiment already exported to txt : Ok ")
+
+    # Generate list with paths of sentiments
     list_sent = Generate_path(Data_Sentiment, Params.Path_Control)
     jsonl = list_sent.paths_jsl(Params.complement_ini, Params.complement_end)
-    print ("The Paths  prediction exported : OK " )
-    
-    #Batch prediction
+    print("The Paths  prediction exported : OK ")
+
+    # Batch prediction
     print("Prediction Batch: ok")
-    batch_prediction_job(Params.project_id, 
-                         Params.location,
-                         
-                         #change depend of the model offsite - onsite
-                         Params.model_id_off_site, 
-                         Params.job_display_name,
-                         Params.input_uri,
-                         Params.output_uri)
+    batch_prediction_job(
+        Params.project_id,
+        Params.location,
+        # change depend of the model offsite - onsite
+        Params.model_id_off_site,
+        Params.job_display_name,
+        Params.input_uri,
+        Params.output_uri,
+    )
